cbe1b71c7bf8e1f49744857f8902571228272e38
Raihan Zaman <raihan.zaman@amadeyr.org>
2019-05-23 11:14:53 +0600

    starting product-list ui

--- src/app/components/category-list/category-list.component.ts
+++ src/app/components/category-list/category-list.component.ts
@@ -1,14 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-category-list',
-  templateUrl: './category-list.component.html',
-  styleUrls: ['./category-list.component.scss'],
+    selector: 'app-category-list',
+    templateUrl: './category-list.component.html',
+    styleUrls: ['./category-list.component.scss'],
 })
 export class CategoryListComponent implements OnInit {
-  private items = ['test', 'test2', 'test3', 'test4', 'test', 'test2', 'test3', 'test4', 'test', 'test2', 'test3', 'test4', 'test', 'test2', 'test3', 'test4'];
-  constructor() { }
+    private items = ['test', 'test2', 'test3', 'test4', 'test',
+                    'test2', 'test3', 'test4', 'test', 'test2',
+                    'test3', 'test4', 'test', 'test2', 'test3',
+                    'test4'];
 
-  ngOnInit() {}
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
 
 }
