edb39933c7d0312a731d177125d27c7de22f0aa9
Raihan Zaman <raihan.zaman@amadeyr.org>
2019-05-27 13:05:49 +0600

    Update for client done

--- src/app/menu/menu.page.ts
+++ src/app/menu/menu.page.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {UpdateService} from '../service/update.service';
+import {AlertController, LoadingController} from '@ionic/angular';
 
 @Component({
     selector: 'app-menu',
@@ -16,19 +17,42 @@ export class MenuPage implements OnInit {
         }
     ];
 
-    constructor(private updateService: UpdateService) {
+    constructor(private updateService: UpdateService,
+                private alertController: AlertController,
+                private loadingController: LoadingController) {
     }
 
     ngOnInit() {
     }
 
-    update() {
+    async update() {
         console.log('update click');
-        this.updateService.index().then().catch((error) => {
-            console.log('updating error');
-            console.log(error);
+        const alert = await this.alertController.create({
+            header: 'Update',
+            message: 'Are you sure you want to <strong>Update</strong>!!!',
+            buttons: [
+                {
+                    text: 'Cancel',
+                    role: 'cancel',
+                    cssClass: 'secondary',
+                    handler: (blah) => {
+                        console.log('Confirm Cancel: blah');
+                    }
+                }, {
+                    text: 'Okay',
+                    handler: (() => {
+                        this.updateService.index().then(() => {
+                            console.log('update done ====');
+                        }).catch((error) => {
+                            console.log('updating error');
+                            console.log(error);
+                        });
+                    })
+                }
+            ]
         });
 
+        await alert.present();
     }
 
 }
