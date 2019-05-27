347f3f8ed0f96f24b48a4b268655cd09f084741c
Raihan Zaman <raihan.zaman@amadeyr.org>
2019-05-27 12:29:38 +0600

    created client model

--- src/app/_models/client.ts
+++ src/app/_models/client.ts
@@ -1,2 +1,21 @@
 export class Client {
+    id: number;
+    name: string;
+    email: string;
+    discount: number;
+    address: string;
+
+    constructor(id: number = 0,
+                name: string = '',
+                email: string = '',
+                discount: number = 0,
+                address: string = '') {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.discount = discount;
+        this.address = address;
+    }
+
+
 }
