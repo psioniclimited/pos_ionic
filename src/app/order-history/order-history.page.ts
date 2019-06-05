21d37d2c6765b03d50afc105aeae238467531736
Raihan <riger400@gmail.com>
2019-06-06 02:48:31 +0600

    starting to work on showing order details in history

--- src/app/order-history/order-history.page.ts
+++ src/app/order-history/order-history.page.ts
@@ -1,5 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {OrderService} from '../service/order.service';
+import {IonInfiniteScroll} from "@ionic/angular";
+import {Order} from "../_models/order";
 
 @Component({
     selector: 'app-order-history',
@@ -7,6 +9,7 @@ import {OrderService} from '../service/order.service';
     styleUrls: ['./order-history.page.scss'],
 })
 export class OrderHistoryPage implements OnInit {
+    @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
     orderCollection: any;
 
     constructor(private orderService: OrderService) {
@@ -31,6 +34,22 @@ export class OrderHistoryPage implements OnInit {
             console.log('error in order fetching while client search');
             console.log(error);
         });
+    }
+
+    async loadData(event: any) {
+        const lastId = this.orderCollection[this.orderCollection.length - 1].id;
+
+        await this.orderService.getOrdersFromDB(lastId).then((data: Order[]) => {
+            for (let i = 0; i < data.length; i++) {
+                this.orderCollection.push(data[i]);
+            }
+        }).catch((error) => {
+            console.log('error in order fetching');
+            console.log(error);
+        });
+
+        event.target.complete();
+
     }
 
 }
